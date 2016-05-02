cbe4876f751de25c9f6485504a4f5b78f0c2f5c8
NStephenson <nickstephenson90@gmail.com>
2016-05-01 21:52:38 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,21 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+  it('should return all people with the favorite food listed', function(){
+    var people = [{
+      name: 'Person 1',
+      favoriteFood: 'Sushi'
+    }, {
+      name: 'Person 2',
+      favoriteFood: 'Ice Cubes'
+    }, {
+      name: 'Hannibal',
+      favoriteFood: 'People'
+    }];
+
+    var results = $filter('favoriteFood')(people, 'People');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Hannibal');
+  });	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -3,8 +3,14 @@ describe('removeAllVowels Filter', function () {
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
+
+  it('should remove the vowels from a string', function(){
+
+    expect(removeAllVowels("Hey, this would taste good.")).toEqual("Hy, ths wld tst gd.");
+    expect(removeAllVowels("aeiou")).toEqual("");
+  });
 
 });
